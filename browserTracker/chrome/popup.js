cdf7c30a5a9629742066b4d19c9b8f17949f9538
NataliaAru <nata2@uw.edu>
2019-03-06 12:47:59 -0800

    Add files via upload
    Architecture

---
+++ browserTracker/chrome/popup.js
@@ -0,0 +1,109 @@
+(function () {
+  //how many sites displayed and how many days to consider
+  var numberOfViews = 10;
+  var days = 1;
+  var server = "http://localhost:8080";
+ //takes in string json object and boolean - to clear or not
+  display = function (data, clearVal) {
+    var json = JSON.parse(data);
+    var old_tbody = document.getElementById("stats_tbody");
+    var tbody = document.createElement("tbody");
+    tbody.setAttribute("id", "stats_tbody");
+    var table = document.getElementById("stats");
+    table.removeChild(old_tbody);
+    table.appendChild(tbody);
+    if (!clearVal) {
+      for (var key in json) {
+        var item = json[key];
+        row = addRow(item["icon"], item["URL"], item["totalTime"], item["pageVisits"]);
+        tbody.appendChild(row);
+      }
+    }
+  }
+  // take in image, and url, time and visits as string, returns a row object
+  addRow = function (image, url, time, visits) {
+    var row = document.createElement("tr");
+    row.appendChild(imageCell(image));
+    row.appendChild(urlCell(url));
+    row.appendChild(getCell(time));
+    row.appendChild(getCell(visits));
+    return row;
+  }
+  //return Cell with image
+  imageCell = function (image) {
+    cell = document.createElement("td");
+    image.height = 10;
+    image.weight = 10;
+    cell.appendChild(image);
+    return cell;
+  }
+  //returns cell with url, url opens in new tab
+  urlCell = function (url) {
+    var urlcell = document.createElement("td");
+    var cite = document.createElement('a');
+    cite.appendChild(document.createTextNode(url));
+    cite.title = "Open link";
+    cite.href = url;
+    cite.target = "_blank";
+    urlcell.appendChild(cite);
+    return urlcell;
+  }
+  //return cell with string val
+  getCell = function (val) {
+    cell = document.createElement("td");
+    cell.appendChild(document.createTextNode(val));
+    return cell;
+  }
+  //gets websites that are stored in the database
+  getWebsites = function () {
+    //not sure what to do with days
+    var val = "/topSites?sites=" + numberOfViews + "&dateRange=" + days + "&currentDate="+Date.now();
+    RequestDatabase(val);
+  }
+  //respose for submit number of websites to show -> create new display
+  doSubmit = function () {
+    var x = document.getElementById("views").value;
+    if (x < 1 || x > 20) {
+      x = 10;
+      alert("number has to be larger than 0 and smaller than 21");
+    }
+    numberOfViews = x;
+    getWebsites();
+  }
+  //get request database and display data
+  RequestDatabase = function (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", server + url);
+    xhr.onload = function () {
+      display(xhr.response, false);
+    }
+    xhr.send();
+  }
+  //clear out database and display no data
+  clear = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", server + "/reset");
+    xhr.onload = function () {
+      display(xhr.response, true);
+    }
+    xhr.send();
+  }
+  //set up buttons and initial view
+  document.addEventListener('DOMContentLoaded', function () {
+    // --- Send request example: ---
+    var server = "http://localhost:8080";
+    var resetButton = document.getElementById('reset');
+    resetButton.onclick = function () {
+      clear();
+    };
+    //deal with radio buttons
+    var radios = document.forms["days"].elements["time"];
+    for (radio in radios) {
+      radios[radio].onclick = function () {
+        days = this.value;
+        getWebsites();
+      }
+    };
+    display(null, false);
+  }, false);
+})();
---
+++ browserTracker/chrome/tracking.js
@@ -0,0 +1,142 @@
+//TODO:
+// - in server.py:
+//     - implement "update with end time"
+//     - change topX to count total time and rank by total time
+// - stretch: figure out how to only query for recordID in updateTimeWithCurrentTab when necessary, not always
+
+var config = config();
+var current = {
+    siteRecordID: null,
+    tabURL: null
+}
+
+function log(url){
+    var deferred = new $.Deferred();
+
+    //this check handles two cases:
+    // - window was unfocused, user clicks tab in window; windows.onFocusChanged
+    //   is fired, followed by tabs.onActivated. This check prevents the
+    //   tabs.onActivated event listener from re-logging the current page
+    // - window was unfocused, user clicks on window element (not on tab), and
+    //   then later clicks on the current tab; events would occur in the same
+    //   order as the above case, and this prevents the tabs.onActivated event
+    //   listener from re-logging the current page
+    if (url == current.tabURL) return deferred.reject();
+    current.tabURL = url;
+
+    console.log("POST");
+    var data = JSON.stringify({
+        url: url,
+        time: Date.now()
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", config.server + "/log");
+    xhr.onload = function() {
+        deferred.resolve(JSON.parse(xhr.response).siteRecordID);
+    }
+    xhr.send(data);
+
+    return deferred.promise();
+}
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    console.log("onActivated");
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        if (tab.status === "complete" && tab.active) {
+            chrome.windows.get(tab.windowId, {populate: false}, function(window) {
+                if (window.focused) {
+                    log(tab.url).done(updatePreviousAndSwitchToCurrent);
+                }
+            });
+        }
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    console.log("onUpdated");
+    if (changeInfo.status === "complete" && tab.active) {
+        chrome.windows.get(tab.windowId, {populate: false}, function(window) {
+            if (window.focused) {
+                log(tab.url).done(updatePreviousAndSwitchToCurrent);
+            }
+        });
+    }
+});
+
+chrome.windows.onFocusChanged.addListener(function (windowId) {
+    console.log("onFocusChanged");
+    if (windowId == chrome.windows.WINDOW_ID_NONE) {
+        //send end time for current record
+        updatePreviousAndSwitchToCurrent(null);
+    } else {
+        //new record
+        chrome.windows.get(windowId, {populate: true}, function(window) {
+            if (window.focused) {
+                chrome.tabs.query({active: true, windowId: windowId}, function (tabs) {
+                    if (tabs[0].status === "complete") {
+                        log(tabs[0].url).done(updatePreviousAndSwitchToCurrent);
+                    }
+                });
+            }
+        });
+    }
+});
+
+setInterval(function() {
+    console.log("interval");
+    chrome.windows.getCurrent(function(window) {
+        if (window.focused) {
+            console.log("window is focused");
+            updateTimeWithCurrentTab();
+        } else {
+            console.log("window is not focused");
+            updatePreviousAndSwitchToCurrent(null);
+        }
+    });
+}, config.updateTimeSeconds * 1000);
+
+chrome.idle.onStateChanged.addListener(function(idleState) {
+    console.log("idle.onStateChanged");
+    if (idleState == "locked") {
+        updatePreviousAndSwitchToCurrent(null);
+    }
+});
+
+//updates time for previous siteRecordID, then updates current.siteRecordID to current siteRecordID
+function updatePreviousAndSwitchToCurrent(siteRecordID) {
+    updateTime(current.siteRecordID); //aka send current time to server
+    console.log("setting current.siteRecordID to " + siteRecordID);
+    current.siteRecordID = siteRecordID || null;
+};
+
+//sends updated "end" time for current.siteRecordID, if current.siteRecordID is not null
+function updateTime(siteRecordID) {
+    if (!current.siteRecordID) return;
+
+    var data = JSON.stringify({
+        siteRecordID: siteRecordID,
+        time: Date.now()
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", config.server + "/update");
+    xhr.send(data);
+}
+
+//updates with current tab -- used if current tab url isn't straightforward to
+//determine in the context that this function is being called from
+function updateTimeWithCurrentTab() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+        if (tabs.length == 1) {
+            var url = tabs[0].url;
+            var data = JSON.stringify({ url: url,});
+            var xhr = new XMLHttpRequest();
+
+            //use POST because I didn't want to send url as parameter in request url
+            xhr.open("POST", config.server + "/recordID");
+            xhr.onload = function() {
+                updatePreviousAndSwitchToCurrent(JSON.parse(xhr.response).siteRecordID);
+            }
+            xhr.send(data);
+        }
+    });
+};
